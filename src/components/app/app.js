493285e999b7b27f86b9a8494d987cc590c0a1b1
Anastasiia Bevziuk <anastasiia@MacBook-Pro-Anastasiia.local>
2021-06-04 15:45:40 +0300

    map to 2d arrey with 0 or X

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Header from '../header/header.js';
 import Field from '../field/field.js';
 import Result from '../result/result.js';
@@ -7,17 +7,26 @@ import TryAgain from '../tryAgain/tryAgain';
 
 import './app.css';
 
-const App =() => {
-    return(
-        <div className='base'>
-            <Header/>
-            <Field/>
-            <Result/>
-            <TryAgain/>
-        </div>
-    )
+export default class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            logics:[[0, null, null],[0, 1, null],[null, null, null]],
+        }
+    }
+    render(){
+        return(
+            <div className='base'>
+                <Header/>
+                <Field logics={this.state.logics}/>
+                <Result/>
+                <TryAgain/>
+            </div>
+        )
+    }
+    
 }
-export default App;
+
 
 
   
--- src/components/field/field.js
+++ src/components/field/field.js
@@ -1,31 +1,93 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './field.css';
 
-const Field = () => {
-    return (
-        <>
-            <table className="tables">
-            <tbody>
-                <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+
+/*function draw (arr){
+   
+    for(let i =0; i<3; i++){
+        for(let k =0; k<3; k++){
+           
+        }
+    }
+}*/
+
+export default class Field extends Component {
+   
+render(){
+    const {logics} = this.props;
+    let elements;
+    //let elementsTwo;
+    //let elementsThree;
+    
+    elements = logics.map((item)=> {
+        item.map((i)=>{
+            if(i === 0){
+            
+                return 'O';
+            }else if(i === 1){
+            
+                return 'X';
+            } return "";
+        });
+        return item;
+    });
+       /* elements = logics[0].map((item) => {
+            if(item === 0){
+                
+                return 'O';
+            }else if(item === 1){
+            
+                return 'X';
+            } return "";
+        });
+        elementsTwo = logics[1].map((item) => {
+            if(item === 0){
+                
+                return 'O';
+            }else if(item === 1){
+            
+                return 'X';
+            } return "";
+        });
+        elementsThree = logics[2].map((item) => {
+            if(item === 0){
+                
+                return 'O';
+            }else if(item === 1){
+            
+                return 'X';
+            } return "";
+        });*/
+    
+       
+        return (
+         <>
+                <table className="tables">
+                <tbody>
+                    <tr>
+                        <td>{elements[0][0]}</td>
+                        <td>{elements[0][1]}</td>
+                        <td>{elements[0][2]}</td>
+                            
                     </tr>
                     <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>{elements[1][0]}</td>
+                        <td>{elements[1][1]}</td>
+                        <td>{elements[1][2]}</td>
                     </tr>
                     <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>{elements[2][0]}</td>
+                        <td>{elements[2][1]}</td>
+                        <td>{elements[2][2]}</td>
                     </tr>
                 </tbody>
-            </table>
-        </>
-    );
+                </table>
+            </>
+        )
+    }
+        
 }
 
-export default Field;
+  
+
